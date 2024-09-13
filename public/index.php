1f5c0770c059b7c00755e58baace40da00989e87
nelly <nelly.babillon@gmail.com>
2024-09-13 03:21:32 +0000

    COUMIT

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -34,21 +35,30 @@ $app->post('/process', function (Request $request, Response $response, $args) {
 // Afficher le formulaire de traitement
 $app->get('/analyze', function (Request $request, Response $response, $args) {
     $view = Twig::fromRequest($request);
+    $defaultSource = <<<EOT
+        Comme son nom l’indique, cette race de poney est originaire des îles Shetland situées au nord de l’Ecosse. Selon la légende, cet animal aurait survécu dans cet archipel depuis l’ère glaciaire. Ce poney pourrait aussi avoir migré de Scandinavie vers les îles Shetland il y a 8000 ans. C’est la raison pour laquelle il arbore un côté très rustique et un pelage épais capable de résister à des conditions climatiques extrêmes. C’est le cheval le plus puissant et résistant au monde. Certaines découvertes fossiles tendent à dire que son origine remonterait à l’Holocène, une période géologique interglaciaire. Le poney Shetland aurait donc pour lointains ancêtres les poneys nordiques de la toundra d’Europe du Nord. Il pourrait enfin être un descendant des chevaux naufragés de l’invincible Armada lors de la guerre anglo-espagnole de 1585. À l’heure actuelle, rien ne permet d’affirmer l’une ou l’autre de ces hypothèses.
+    EOT;
+
     return $view->render($response, 'analyze.html.twig', [
-        'name' => 'John',
+        "defaultSource" => $defaultSource,
+        "defaultModel" => "Xenova/bert-base-multilingual-cased-ner-hrl"
     ]);
 });
 
 // Récupèrer les informations du formulaire
 $app->post('/analyze', function (Request $request, Response $response, $args) {
     $source = $request->getParsedBody()['source'];
-    $classifier = pipeline('sentiment-analysis');
-    $result = $classifier($source);
+    set_time_limit(120);
+    ini_set('memory_limit', '512M');
+    $summarizer = pipeline('summarization', 'Xenova/distilbart-cnn-6-6');
+
+    $summary = $summarizer($source, maxNewTokens: 512, temperature: 0.7);
+    var_dump($summary);
     // Rendu de la vue
     $view = Twig::fromRequest($request);
     return $view->render($response, 'analyze-post.html.twig', [
         'source' => $source,
-        'result' => $result
+        'output' => $summary
     ]);
 });
 
